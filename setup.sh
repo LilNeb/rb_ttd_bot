e624d8f5c0ea9a6509337f97ca5f5af3aebee8d0
LilNeb <117268696+LilNeb@users.noreply.github.com>
2024-06-03 18:50:56 +0200

    .

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install python
 brew install git
-pip install -r requirements.txt
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install python
+
+# pip install -r requirements.txt
+
+# add these lines to your .bashrc or .bash_profile
+# alias python=python3
+# alias python=/opt/homebrew/opt/python@3.12/libexec/bin/python
